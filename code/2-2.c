ab91b7b9957758953dedf257f1aece60a95c05a3
Neo6424 <129708429+Neo6424@users.noreply.github.com>
2026-05-08 10:09:32 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 struct access_rights {
-    unsigned char execute :1; // 1비트 할당
-    unsigned char write   :1; // 1비트 할당
-    unsigned char read    :1; // 1비트 할당
+    unsigned char execute :1; 
+    unsigned char write   :1;
+    unsigned char read    :1;
 };
 
 union file_access_rights {
@@ -14,10 +14,9 @@ union file_access_rights {
 int main() {
     union file_access_rights some_file = {0};
     
-    some_file.rights.read = 1;      // 001 (2진수)
-    some_file.rights.write = 0;     // 000 (2진수)
-    some_file.rights.execute = 1;   // 100 (2진수)
-    
-    // 비트들이 합쳐져서 하나의 숫자로 출력됩니다.
+    some_file.rights.read = 1;      
+    some_file.rights.write = 0;     
+    some_file.rights.execute = 1;  
+
     printf("current mode: %u\n", some_file.int_value);
 }
