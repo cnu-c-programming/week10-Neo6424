60655b55bac43e013fb2f7e8f55043bd2740b1b2
Neo6424 <129708429+Neo6424@users.noreply.github.com>
2026-05-12 13:14:40 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -4,18 +4,16 @@ struct S1 {
     char c;
     int i;
     long long l;
-}
+};
 
 struct S2 {
     char c;
     int i;
     long long l;
-} _attribute_((packed));
+} __attribute__((packed));
 
-
-int main()
-{
+int main() {
     struct S1 s1;
     struct S2 s2;
-    printf("%zu %zu\n", sizeof(s1),sizeof(s2) );
+    printf("%zu %zu\n", sizeof(s1), sizeof(s2));
 }
